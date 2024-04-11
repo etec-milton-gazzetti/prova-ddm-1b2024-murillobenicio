a8cdf39945690e0336a05a30ef1c780cc5a56b2d
murillobenicio <134696484+murillobenicio@users.noreply.github.com>
2024-04-11 14:51:53 +0000

    Tree_Commit

--- router.js
+++ router.js
@@ -72,5 +72,5 @@ export default function App() {
     <NavigationContainer>
       <MyTabs/>
     </NavigationContainer>
-  );
+  )
 }
--- screens/index.js
+++ screens/index.js
@@ -3,6 +3,7 @@ import { useCallback } from 'react';
 import { Text, View, Pressable, Image, ScrollView } from 'react-native';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
+
 import Templo from '../components/Templo.jpg';
 import Roupas from '../components/Roupas.jpg';
 import Filosofos from '../components/Filosofos.webp';
@@ -13,11 +14,11 @@ import styles from './style';
 
 export default function Inicio({ navigation }) {
 
-// SplashScreen.preventAutoHideAsync();
+  // SplashScreen.preventAutoHideAsync();
 
-   const [fontsLoaded, fontError] = useFonts({
-     'Inter-Bold': require('../assets/fonts/Inter-Bold.otf'),
-   });
+  const [fontsLoaded, fontError] = useFonts({
+    'Inter-Bold': require('../assets/fonts/Inter-Bold.otf'),
+  });
 
   // const onLayoutRootView = useCallback(async () => {
   //   if (fontsLoaded || fontError) {
@@ -35,13 +36,16 @@ export default function Inicio({ navigation }) {
   // }
   return (
     <ScrollView style={styles.container}>
-      <Text style={{fontFamily: 'Inter-Bold',color: '#c6d6d6',textAlign: 'center',marginTop: '30%',marginBottom: '10%',fontSize: 48,}}>Grécia</Text>
-      <View>
+      <Text style={{ fontFamily: 'Inter-Bold', color: '#c6d6d6', textAlign: 'center', marginTop: '30%', marginBottom: '10%', fontSize: 48, }}>Grécia</Text>
+      <ScrollView>
         <Text style={styles.title2}>Veja algumas representações</Text>
-        <Image style={styles.Img} source={Templo}/>
-        <Image style={styles.Img} source={Roupas}/>
-        <Image style={styles.Img} source={Filosofos}/>
-      </View>
+        <Image style={{
+          height: '30%',
+          width: '100%',
+        }} source={Templo} />
+        <Image source={Roupas} />
+
+      </ScrollView>
     </ScrollView>
   );
 }
