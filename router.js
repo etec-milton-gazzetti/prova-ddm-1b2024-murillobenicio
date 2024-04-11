7095bb35f48d83f96a228f4383ced439c84be114
murillobenicio <134696484+murillobenicio@users.noreply.github.com>
2024-04-11 13:06:54 +0000

    First_Commit

---
+++ router.js
@@ -0,0 +1,78 @@
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Home from './screens/index';
+import Sobre from './screens/sobre';
+import Form from './screens/form';
+
+const HomeStack = createNativeStackNavigator();
+
+function HomeStackScreen() {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Inicio" component={Home} />
+    </HomeStack.Navigator>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        tabBarActiveTintColor: '#dee7e7',
+        tabBarInactiveTintColor: '#a92727',
+        tabBarStyle: { backgroundColor: '#282832' },
+      }}>
+      <Tab.Screen
+        name="Sobre"
+        component={Sobre}
+        options={{
+          tabBarLabel: 'Sobre',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="bell" color={color} size={size} />
+          ),
+        }}
+      />
+
+      <Tab.Screen
+        name="Início"
+        component={HomeStackScreen}
+        options={{
+          headerShown: false,
+
+          tabBarLabel: 'Início',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+
+      <Tab.Screen
+        name="História"
+        component={Form}
+        options={{
+          tabBarLabel: 'História',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <HomeStack.Navigator>
+        <HomeStack.Screen name="Home" component={Home} />
+      </HomeStack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ screens/index.js
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import { useCallback } from 'react';
+import { Text, View, Pressable, Image, ScrollView } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import Templo from '../components/Templo.jpg';
+import Roupas from '../components/Roupas.jpg';
+import Filosofos from '../components/Filosofos.webp';
+
+
+// You can import from local files
+import styles from './style';
+
+export default function Inicio({ navigation }) {
+
+// SplashScreen.preventAutoHideAsync();
+
+  // const [fontsLoaded, fontError] = useFonts({
+  //   'Inter-Bold': require('../assets/fonts/Inter-Bold.otf'),
+  // });
+
+  // const onLayoutRootView = useCallback(async () => {
+  //   if (fontsLoaded || fontError) {
+  //     await SplashScreen.hideAsync();
+  //     // console.log(fontsLoaded)
+  //     // console.log(fontError)
+  //   }
+  // }, [fontsLoaded, fontError]);
+
+  // if (!fontsLoaded && !fontError) {
+  //     console.log(fontsLoaded)
+  //     console.log(fontError)
+
+  //   return null;
+  // }
+  return (
+    <ScrollView style={styles.container}>
+      <Text style={{fontFamily: 'Inter-Bold',color: '#c6d6d6',textAlign: 'center',marginTop: '30%',marginBottom: '10%',fontSize: 48,}}>Grécia</Text>
+      <View>
+        <Text style={styles.title2}>Veja algumas representações</Text>
+        <Image style={styles.Img} source={Templo}/>
+        <Image style={styles.Img} source={Roupas}/>
+        <Image style={styles.Img} source={Filosofos}/>
+      </View>
+    </ScrollView>
+  );
+}
+
